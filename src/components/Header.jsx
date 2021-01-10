fc1f6523ae0b191b842b9d09d53b8a1d0ed4ed33
Jonathan Chen <jonathanchen716@144.121.140.150.lightower.net>
2021-01-10 13:42:12 -0500

    Finish keeper app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
+
+function Header() {
+  return (
+    <header>
+      <h1><EmojiObjectsIcon />Jonathan's Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
